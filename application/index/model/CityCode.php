aed406cebc073df965140b427bd6f4d8f93d9111
zdd00 <962375670@qq.com>
2018-01-18 23:59:26 +0800

    城市码

---
+++ application/index/model/CityCode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZDD
+ * Date: 2018/1/18
+ * Time: 23:41
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class CityCode extends Model
+{
+
+}
